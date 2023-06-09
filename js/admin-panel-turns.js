21691221c9ff7798e172588e4f635cf5704c985d
Homet Matias <mhometherrera@gmail.com>
2023-06-09 14:57:47 -0300

    Se agregrego el boton de modificar turnos y es funcional, se agrego diseño a la pagina de admins turns y se termino el sistema de turnos

--- js/admin-panel-turns.js
+++ js/admin-panel-turns.js
@@ -1,4 +1,5 @@
 const tableTurns = document.getElementById('table-turns');
+let editTurns;
 
 function mostrarTurnos(turnRender) {
   
@@ -19,8 +20,8 @@ function mostrarTurnos(turnRender) {
       <td>${turnTable.date} </td>
       <td>${turnTable.hour} </td>
       <td>
-        <button class="btn btn-success" px-1" onclick="editarTurno(${turnTable.id})"> <i class="fa-solid fa-pen-to-square"></i> </i></button>
-        <button class="btn btn-danger" px-1" onclick="borrarTurnos(${turnTable.id})"> <i class="fa-solid fa-trash"> </i></button>
+        <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#editModal" onclick="editarTurno(${turnTable.id})"> <i class="fa-solid fa-pen-to-square"></i> </i></button>
+        <button class="btn btn-danger" onclick="borrarTurnos(${turnTable.id})"> <i class="fa-solid fa-trash"> </i></button>
       </td>
       </tr>`;
   
@@ -31,17 +32,66 @@ function mostrarTurnos(turnRender) {
   mostrarTurnos(turns);
 
   function borrarTurnos(idTurnos) {
+    const indexTurno = turns.findIndex(turn => turn.id === idTurnos);
   
-    const idTurnoTable = turns.findIndex(turn => turn.id === idTurnos);
-
-    if (idTurnoTable === -1) return swal('Error', 'El usuario no se pudo encontrar', 'error');
+    if (indexTurno === -1) {
+      return swal('Error', 'El turno no se pudo encontrar', 'error');
+    }
   
-    turns.splice(idTurnoTable, 1);
+    turns.splice(indexTurno, 1);
   
     localStorage.setItem('turnos', JSON.stringify(turns));
   
     mostrarTurnos(turns);
-  
   }
+  
 
+  function editarTurno(id) {
+    editTurns = id;
+  
+    const turnoEditado = turns.find(turn => turn.id === id);
+    if (!turnoEditado) {
+      swal('Error', 'El turno no se pudo encontrar', 'error');
+      return;
+    }
+  
+    const form = document.getElementById('form-turn');
+    form.nombre.value = turnoEditado.nombre;
+    form.mail.value = turnoEditado.mail;
+    form.dni.value = turnoEditado.dni;
+    form.medico.value = turnoEditado.medico;
+    form.date.value = turnoEditado.date;
+    form.hour.value = turnoEditado.hour;
+  }
+  
+  function guardarTurnoEditado() {
+    const form = document.getElementById('form-turn');
+    const nombre = form.nombre.value;
+    const mail = form.mail.value;
+    const dni = form.dni.value;
+    const medico = form.medico.value;
+    const date = form.date.value;
+    const hour = form.hour.value;
+  
+    const indexTurno = turns.findIndex(turn => turn.id === editTurns);
+    if (indexTurno === -1) {
+      swal('Error', 'El turno no se pudo encontrar', 'error');
+      return;
+    }
+  
+    const turnoExistente = turns[indexTurno];
+    turnoExistente.nombre = nombre;
+    turnoExistente.mail = mail;
+    turnoExistente.dni = dni;
+    turnoExistente.medico = medico;
+    turnoExistente.date = date;
+    turnoExistente.hour = hour;
+  
+    localStorage.setItem('turnos', JSON.stringify(turns));
+  
+    const editModal = bootstrap.Modal.getInstance(document.getElementById('editModal'));
+    editModal.hide();
+  
+    mostrarTurnos(turns);
+  }
   
--- js/turns.js
+++ js/turns.js
@@ -6,27 +6,37 @@ const formTurns = document.getElementById('form-turn');
 
 function agendarTurnos(event) {
     event.preventDefault();
-
-    const turnsHTML = {
-        nombre: event.target.elements.nombre.value,
-        mail: event.target.elements.mail.value,
-        date: event.target.elements.date.value,
-        dni: event.target.elements.dni.valueAsNumber,
-        hour: event.target.elements.hour.value,
-        medico: event.target.elements.medico.value,
-        descripcion: event.target.elements.descripcion.value,
-        id: Date.now()
+  
+    const nuevoTurno = {
+      nombre: event.target.elements.nombre.value,
+      mail: event.target.elements.mail.value,
+      date: event.target.elements.date.value,
+      dni: event.target.elements.dni.valueAsNumber,
+      hour: event.target.elements.hour.value,
+      medico: event.target.elements.medico.value,
+      descripcion: event.target.elements.descripcion.value,
+      id: Date.now()
+    };
+  
+    const indexTurnoExistente = turns.findIndex(turn => turn.id === nuevoTurno.id);
+  
+    if (indexTurnoExistente !== -1) {
+      turns[indexTurnoExistente] = nuevoTurno;
+    } else {
+      turns.push(nuevoTurno);
     }
-
-    turns.push(turnsHTML);
-
+  
     localStorage.setItem('turnos', JSON.stringify(turns));
-
+  
     event.target.reset();
     event.target.elements.nombre.focus();
-
-    swal(`Felicidades ${turnsHTML.nombre}`, 'Su turno se agendo con exito', 'success');
-}
+  
+    swal(`Felicidades ${nuevoTurno.nombre}`, 'Su turno se agendó con éxito', 'success');
+  
+    mostrarTurnos(turns);
+  }
+  
+  
 
 formTurns.addEventListener('submit', (event) => agendarTurnos(event));
 
