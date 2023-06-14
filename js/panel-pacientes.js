51f9d9ab424b46b85a83ee1b48328256dfc3f4c7
Rocio <mprd98@gmail.com>
2023-06-14 14:22:36 -0300

    se modifico el panel de pacientes

--- js/panel-pacientes.js
+++ js/panel-pacientes.js
@@ -1,7 +1,7 @@
 const turno = JSON.parse(localStorage.getItem('turnos'));
 const user = JSON.parse(localStorage.getItem('currentUser'));
-const turnosPacientes=turnos.filter((turno) => turno.paciente===user.especialidad);
-const turnosRest=turnos.filter((turno) => turno.paciente!=user.especialidad);
+const turnosPacientes=turnos.filter((turno) => turno.paciente===user.name);
+const turnosRest=turnos.filter((turno) => turno.paciente!=user.name);
 
 
 
@@ -15,8 +15,7 @@ const dias = [
         'Sábado',
       ];
 
-let editIndex;
-let turnoEditado={};
+
 
 const tableBodyHTML = document.getElementById("tableBody");
 const nombrePaciente =document.getElementById("nombrePaciente")
@@ -46,18 +45,13 @@ function renderizarTabla(arrayturnos)
     tableBodyHTML.innerHTML += `<tr>
                                     
                                     <th scope="row">${posicion}</th>
-                                    <td>${turno.nombre}</td>
+                                    <td>${user.name}</td>
                                     <td>${turno.dni}</td>
                                     <td>${dias[numeroDia]}</td>
                                     <td>${turno.date}</td>
                                     <td>${turno.hour}</td>
                                     <td>${turno.medico}</td>
-                                    <td>${turno.descripcion}</td>
-                                    <td>
-                                    <button class="btn btn-primary" onclick="borrarTurno(${turno.id})"><i class="fa-solid fa-trash"></i></button>
-                                    <button class="btn btn-info" data-bs-toggle="modal" data-bs-target="#ModalTurnos" onclick="editarTurno(${turno.id})"><i class="fa-solid fa-edit"></i></button>
-                                    </td>
-                                    
+                                    <td>${turno.descripcion}</td>    
                                 </tr>`;
 })
 }
@@ -67,68 +61,11 @@ function TodoslosTurnos()
     renderizarTabla(turnosPacientes);
 }
 
-function editarTurno(id)
-{
-
-let turno=turnosPacientes.find((turnof,idx)=>{
-  if(turnof.id=== id){
-  editIndex= idx;
-  return true;
-  }
-});
-
-let el= formTurnos.elements;
-
-el.nombre.value=turno.nombre;
-el.dni.value= turno.dni;
-//el.dia.value= turno.dia;
-el.fecha.value= turno.date;
-el.hora.value= turno.hour;
-el.descripcion.value= turno.descripcion;
-
-turnoEditado={
-    paciente: turno.paciente,
-    id: turno.id,
-    mail: turno.mail
-}
 
 
-};
-
-function guardarTurno()
-{
-    let el= formTurnos.elements;
-    turnoEditado.nombre= el.nombre.value
-    turnoEditado.date= el.fecha.value
-    turnoEditado.dni=el.dni.value
-    turnoEditado.hour= el.hora.value
-    turnoEditado.descripcion=el.descripcion.value
-
-    console.log(turnoEditado);
-const index=turnos.findIndex(turno=>turno.id===turnoEditado.id)
-const index2=turnosPacientes.findIndex(turno=>turno.id===turnoEditado.id)
-turnos[index]=turnoEditado;
-turnosPacientes[index2]=turnoEditado;
 
 
-localStorage.setItem('turnos', JSON.stringify(turnos));
 
-
-renderizarTabla(turnosPacientes);
-editIndex=undefined;
-
-}
-
-function borrarTurno(id)
-{
-
-    const idx=turnos.findIndex(turno => turno.id===id);
-    const idx2=turnosPacientes.findIndex(turno => turno.id===id);
-    turnos.splice(idx,1);
-    turnosPacientes.splice(idx2,1);
-    localStorage.setItem('turnos', JSON.stringify(turnos));
-    renderizarTabla(turnosPacientes);
-}
 
 
 function ordenar(order)
